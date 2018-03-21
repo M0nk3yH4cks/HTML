8f0e4989cf43d236517423f8007e3080b19ea731
Cossali <fuk@gmail.com>
2018-03-21 11:34:51 +0100

    Added Mail control

--- digitalFirm/main.js
+++ digitalFirm/main.js
@@ -1,33 +1,51 @@
 console.log("Debug Started");
 
 function emailVerification(field){
+    var checkedValue = document.getElementById("checkedTest");
     console.log(field.value);
     var emailText = field.value;
 
+    if(emailText.substring(0,emailText.indexOf("@") != ""))
     switch (emailText.substring(emailText.indexOf("@"),)) {
         case "@gmail.com":
-            console.log("passed");            
+            console.log("passed");  
+            checkedValue.className = "fa fa-check";  
+            checkedValue.style.color = "green";        
             break;
 
         case "@hotmail.it":
-            console.log("passed");            
+            console.log("passed");  
+            checkedValue.className = "fa fa-check";  
+            checkedValue.style.color = "green";               
             break;
 
         case "@outlook.com":
-            console.log("passed");            
+            console.log("passed");       
+            checkedValue.className = "fa fa-check";  
+            checkedValue.style.color = "green";          
             break;  
 
         case "@outlook.it":
-            console.log("passed");            
+            console.log("passed"); 
+            checkedValue.className = "fa fa-check";  
+            checkedValue.style.color = "green";                
             break;   
 
         case "@yahoo.it":
-            console.log("passed");            
+            console.log("passed");    
+            checkedValue.className = "fa fa-check";  
+            checkedValue.style.color = "green";             
             break;      
 
         default:
+        checkedValue.className = "fa fa-times";
+        checkedValue.style.color = "red";
         console.log("Not Passed");        
             break;
+    }
+    else {
+        checkedValue.className = "fa fa-times";
+        checkedValue.style.color = "red";
     }
     
 }
